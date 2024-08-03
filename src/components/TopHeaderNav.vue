223382831b0dd66f17c5b144351fa3f7e1719015
Jason Lyman <jason@lymannuthouose.com>
2024-08-02 20:58:55 -0700

    Initial commit, added SCSS and header to basic Vite build

---
+++ src/components/TopHeaderNav.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+  <header class="top-header">
+    <div class="container">
+      <h1>Jason Lyman<br /><small>Web Designer & Developer</small></h1>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+</template>
---
+++ src/main.js
@@ -0,0 +1,17 @@
+// import './assets/main.css'
+import './assets/scss/_variables.scss'
+import './assets/scss/_mixins.scss'
+import './assets/scss/_base.scss'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
